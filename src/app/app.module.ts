a4d5fe7d812ba21270fed24fd6ca64eb17c5f66c
Kristin Beltz <beltz08@me.com>
2020-07-17 22:36:45 -0500

    Add address form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestFormComponent } from './test-form/test-form.component';
+import { AddressFormComponent } from './common/address-form/address-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestFormComponent
+    TestFormComponent,
+    AddressFormComponent
   ],
   imports: [
     BrowserModule,
